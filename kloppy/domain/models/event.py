c694cecbe31cfb28690b7e7b3d27014515d49875
lodevt <lode.van.tente@gmail.com>
2025-01-17 09:41:07 +0100

    return dataset after adding synthetic events
    (cherry picked from commit d738e317eaef5b5b7a29f108784b92cbaf406ab1)

--- kloppy/domain/models/event.py
+++ kloppy/domain/models/event.py
@@ -1196,7 +1196,7 @@ class EventDataset(Dataset[Event]):
             raise KloppyError(
                 f"Not possible to generate synthetic {event_type_}"
             )
-        synthetic_event_generator.add_synthetic_event(self)
+        return synthetic_event_generator.add_synthetic_event(self)
 
 
 __all__ = [
